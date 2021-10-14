caa0c04d25f77e67bdda51f9613b0473bc3e5434
JuanMoli111 <hombrebota@hotmail.com>
2021-10-13 21:54:38 -0300

    tp3 ej2 resuelto

--- Cursada/src/TP2/ColaGenerica.java
+++ Cursada/src/TP2/ColaGenerica.java
@@ -11,11 +11,11 @@ public class ColaGenerica<T> {
     }
 
 
-
     public T desencolar()
     {
-        T elem = datos.elemento(0);
-        datos.eliminarEn(0);
+    
+        T elem = datos.elemento(1);
+        datos.eliminarEn(1);
         
         return elem;
     }
--- Cursada/src/TP3/ArbolBinario.java
+++ Cursada/src/TP3/ArbolBinario.java
@@ -1,5 +1,6 @@
 package TP3;
 
+import TP2.ColaGenerica;
 import TP2.ListaEnlazadaGenerica;
 import TP2.ListaGenerica;
 
@@ -131,13 +132,47 @@ public class ArbolBinario<T> {
 
 	}
 
-
 	public void recorridoPorNiveles() {
-		
+
+
+		if(!this.esVacio())
+		{
+
+			//Crear una cola
+			ColaGenerica<ArbolBinario<T>> Cola = new ColaGenerica<>();
+			//Encolar la raiz y el null que sirve de marca para el fin del nivel
+			Cola.encolar(this);
+			Cola.encolar(null);
+
+			//Mientras haya elementos 
+			while(!Cola.esVacia()){
+
+				//Saca un elemento
+				ArbolBinario<T> elem = Cola.desencolar();
+
+				//Comprobar si es un elemento o una marca de fin de nivel
+				if(elem != null)
+				{
+					//Proceso el dato
+					System.out.print(elem.getDato() + " ");
+
+					//Encola los hijos si corresponde
+					if(elem.tieneHijoIzquierdo())
+						Cola.encolar(elem.getHijoIzquierdo());
+					if(elem.tieneHijoDerecho())
+						Cola.encolar(elem.getHijoDerecho());
+				}
+				else
+					if(!Cola.esVacia())
+					{
+						System.out.println();
+						Cola.encolar(null);
+						
+					}
+				
+			}
+		}
 	}
-
-
-
 	
 
 	public ListaGenerica<T> frontera() {
@@ -152,11 +187,53 @@ public class ArbolBinario<T> {
 	public int contarHojas() {
 
 
+		//Si es vacio retornar cero
+		if(this.esVacio()) return 0;
+		//Si no tiene hijos es un arbol hoja, retornar uno
+		else if(!this.tieneHijoIzquierdo() && !this.tieneHijoDerecho())	return 1;
+		else
+		{
+			int n = 0;
 
+			if(tieneHijoDerecho())
+			{
+				n += this.getHijoDerecho().contarHojas(); 
+			}
 
-		return 0;
+			if(tieneHijoIzquierdo())
+			{
+				n += this.getHijoIzquierdo().contarHojas(); 
+			}
+			
+			return n;
+		}
+
 	}
 
-	
+
+
+	public ArbolBinario<T> espejo()
+	{
+		//Si es vacio retornar null	
+		if(this.esVacio()) return null;
+		//Caso base si es una hoja retornar la misma hoja
+		if(!this.tieneHijoIzquierdo() && !this.tieneHijoDerecho()) return this;
+		else
+		{
+			//Si es un nodo con hijos, crear un arbol cargarle este nodo
+			ArbolBinario<T> ArbolEspejo = new ArbolBinario<>(this.getDato());
+
+
+			//Procesar los hijos, si hay hijo izq, agregar al arbol espejo como hijo derecho, el hijo izq invocando el metodo espejo (recursion) y viceversa.
+			if(this.tieneHijoIzquierdo()) ArbolEspejo.agregarHijoDerecho(this.getHijoIzquierdo().espejo());
+
+			if(this.tieneHijoDerecho())  ArbolEspejo.agregarHijoIzquierdo(this.getHijoDerecho().espejo());
+			
+			//Retornar el arbol espejado
+			return ArbolEspejo;
+		}
+	}
+
+
 
 }
